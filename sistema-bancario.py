7685e534815ede8109f155d8c4ba85a62bf934a3
Rafael de Lima Rueckl <rafael.rueckl@hotmail.com>
2025-06-04 08:40:54 -0300

    first commit

---
+++ sistema-bancario.py
@@ -0,0 +1,56 @@
+import textwrap
+
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
+def depositar(saldo, valor, extrato, /):
+    #complete o código
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    #complete o código
+
+def exibir_extrato(saldo, /, *, extrato):
+    #complete o código
+
+def criar_usuario (usuarios):
+    #complete o código
+
+def filtrar_usuario(cpf, usuarios):
+    #complete o código
+
+def criar_conta(agencia, numero_conta, usuarios):
+    #complete o código
+
+def listar_contas(contas):
+    #complete o código
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            saldo 
+
+main()
