ba49da41cdc5ec80659e4834aa361dc34a6e62d1
Rafael de Lima Rueckl <rafael.rueckl@hotmail.com>
2025-06-12 08:41:44 -0300

    Contas

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -23,20 +23,25 @@ def depositar(saldo, valor, extrato, /):
     return saldo,extrato
 
 def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+
     if valor > 0:
         excedeu_saldo = valor > saldo
         excedeu_o_limite = limite > 500
         excedeu_o_limite_de_saques = limite_saques > 3
         if excedeu_o_limite:
             print("Voce excedeu o limite")
+
         elif excedeu_o_limite_de_saques:
             print("Voce excedeu o limite de saques")
+
         elif excedeu_saldo:
             print("Voce excedeu o saldo")
+
         elif valor>0:
             saldo -= valor
             extrato +=f"Saque:R${valor:.2f}\n"
             numero_saques += 1
+
         else:
             print("A operação falhou")
         return saldo,extrato
@@ -48,17 +53,39 @@ def exibir_extrato(saldo, /, *, extrato):
     print("\n=======================")
     
 
-#def criar_usuario (usuarios):
-    #complete o código
+def criar_usuario (usuarios):
+    cpf = int(input("Informe seu cpf (apenas números): "))
+    usuario = filtrar_usuario(cpf, usuarios)
+    if usuario:
+        print("Este usuário já existe.")
+        return
+    nome = input("Digite o seu nome: ").lower()
+    data_nascimento = input("Digite sua data de nascimento (em formato dd-mm-aaaa): ")
+    endereco = input("Digite o seu endereço (em forato de: logradouro, número-bairro-cidade/sigla do estado): ")
 
-#def filtrar_usuario(cpf, usuarios):
-    #complete o código
+def filtrar_usuario(cpf, usuarios):
+    usuarios = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios[0] if usuarios else None
 
-#def criar_conta(agencia, numero_conta, usuarios):
-    #complete o código
+def criar_conta(agencia, numero_conta, usuarios):
+   cpf = input("Digite o CPF do titular da conta: ")
+   usuario = filtrar_usuario(cpf, usuarios)
+   if usuario:
+       print("Conta criada com sucesso")
+       return {
+            "agência": agencia,
+           "número da conta": numero_conta,
+           "usuário": usuario
+       }
+   else:
+       print("Usuário não encontrado. Não foi possível criar uma conta.")
+       return None
 
-#def listar_contas(contas):
-    #complete o código
+def listar_contas(contas):
+    for conta in contas:
+        print(f"Número da agência é {conta['agencia']}")
+        print(f"Número da conta é {conta['numero_conta']}")
+        print(f"O titular é {conta['usuario']['nome']}")
 
     def main():
         LIMITE_SAQUES = 3
